c23b0ae8e3b2b998a1063b17084a071bab9b5f3f
ImGonnaKMS <xalienek@gmail.com>
2024-04-14 11:14:50 +0200

    idk

--- messengerTCP/MainWindow.xaml.cs
+++ messengerTCP/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Net.Sockets;
 using System.Text;
 using System.Net;
+using System.Threading.Tasks;
+using System.IO;
 
 namespace messengerTCP
 {
@@ -13,34 +15,78 @@ namespace messengerTCP
         public MainWindow()
         {
             InitializeComponent();
-
-
+            Window_Loaded();
         }
 
         TcpClient client;
+        TcpListener server;
+        NetworkStream networkStream;
         string ip = "127.0.0.1";
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void Window_Loaded()
         {
-            
+            server = new TcpListener(IPAddress.Parse(ip), 54321);
+            server.Start();
+            Task.Run(() => StartS());
+        }
+
+        private async Task StartS()
+        {
+            while(true)
+            {
+                TcpClient client = await server.AcceptTcpClientAsync();
+                await Task.Run(() => hClient(client));
+            }
         }
 
         private void btnConnect(object sender, RoutedEventArgs e)
         {
-            ChatBox.Text = "Connecting to server...";
+            ChatBox.Text = "Connecting...";
             client = new TcpClient(ip, int.Parse(PortNumber.Text));
+            networkStream = client.GetStream();
             if(client.Connected)
             {
                 ChatBox.Text = "Connected to server";
             }
+            Task.Run(() => StartC());
         }
 
-        private void btnSend(object sender, RoutedEventArgs e)
+        async Task StartC()
+        {
+            while(true)
+            {
+                byte[] data = new byte[256];
+                int bytes = await networkStream.ReadAsync(data, 0, data.Length);
+                string message = Encoding.ASCII.GetString(data, 0, bytes);
+                ChatBox.Text += "A " + message + "\n";
+            }
+        }
+
+        private async void btnSend(object sender, RoutedEventArgs e)
         {
 
-            NetworkStream networkStream = client.GetStream();
-            byte[] sendBuffer = Encoding.ASCII.GetBytes(ChatInput.Text);
-            networkStream.Write(sendBuffer, 0, sendBuffer.Length);
+            string message = ChatInput.Text;
+            if(!string.IsNullOrWhiteSpace(message))
+            {
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                await networkStream.WriteAsync(data,0, data.Length);
+                ChatBox.Text += "A " + message + "\n";
+            }
+            ChatBox.Text += string.Empty + "\n";
+        }
+
+        private async Task hClient(TcpClient client)
+        {
+            using (NetworkStream nStream = client.GetStream())
+            {
+                while(true)
+                {
+                    byte[] data = new byte[256];
+                    int bytes = await nStream.ReadAsync(data, 0, data.Length);
+                    string message = Encoding.ASCII.GetString(data, 0, bytes);
+                    ChatBox.Text += "A " + message + "\n";
+                }
+            }
         }
 
         private void btnDisconnect(object sender, RoutedEventArgs e)
@@ -50,70 +96,3 @@ namespace messengerTCP
         }
     }
 }
-
-/*
-class Client1
-    {
-        static void Main()
-        {
-            string hostName = Dns.GetHostName();
-            IPHostEntry ipEntry = Dns.GetHostEntry(hostName);
-
-            foreach (IPAddress ipAddress in ipEntry.AddressList)
-            {
-                Console.WriteLine($"IP Address: {ipAddress}");
-            }
-
-            TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 12345);
-            listener.Start();
-
-            Console.WriteLine("Client 1 is waiting for a connection...");
-
-            TcpClient client = listener.AcceptTcpClient();
-            Console.WriteLine("Client 1 connected to Client 2");
-
-            Thread receiveThread = new Thread(() => ReceiveData(client));
-            receiveThread.Start();
-
-            while (true)
-            {
-                string message = Console.ReadLine();
-                SendData(client, message);
-            }
-        }
-
-        static void ReceiveData(TcpClient client)
-        {
-            try
-            {
-                NetworkStream networkStream = client.GetStream();
-                byte[] receiveBuffer = new byte[256];
-
-                while (true)
-                {
-                    int bytesRead = networkStream.Read(receiveBuffer, 0, receiveBuffer.Length);
-                    string receivedMessage = Encoding.ASCII.GetString(receiveBuffer, 0, bytesRead);
-                    Console.WriteLine("Client 2: " + receivedMessage);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error receiving data: {ex.Message}");
-            }
-        }
-
-        static void SendData(TcpClient client, string message)
-        {
-            try
-            {
-                NetworkStream networkStream = client.GetStream();
-                byte[] sendBuffer = Encoding.ASCII.GetBytes(message);
-                networkStream.Write(sendBuffer, 0, sendBuffer.Length);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error sending data: {ex.Message}");
-            }
-        }
-    }
- */
